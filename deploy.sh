05fee68c71abf6154bda617e94a0127171912dd6
Marc Ortlieb <windtalkerz@me.com>
2018-11-23 19:01:59 +0100

    + inject secrets + env vars

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
+############################
+# make build
+############################
 react-scripts build
 cd ./build
+
+############################
+# deploy & link alias domain
+############################
 now && now alias
+
+cd ..
+############################
+# inject secrets (gitignored)
+############################
+yarn inject-secrets 
+# ../inject-secrets.sh NOT WORKING -  ./inject-secrets.sh: Permission denied - error Command failed with exit code 126.
